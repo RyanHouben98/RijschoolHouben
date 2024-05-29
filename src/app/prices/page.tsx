d44ef4eec73949499b35cc158d45cf135d398d9c
Ryan Houben <connect@ryanhouben.com>
2024-05-29 17:22:43 +0200

    Start with prices

--- src/app/prices/page.tsx
+++ src/app/prices/page.tsx
@@ -1,3 +1,129 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+
 export default function Page() {
-  return <h1>Prices</h1>;
+  return (
+    <>
+      <div className="pt-32 pb-12">
+        <div className="container mx-auto flex flex-col gap-4">
+          <h1>Onze Prijzen</h1>
+          <Separator />
+          <div className="flex flex-col gap-4">
+            <h2>Pakketten</h2>
+            <div className="grid grid-cols-4 gap-4">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Pakket 35</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>
+                    Pakket 30 rijlessen (60 min. & examen) Theorieboek €2015,-
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Pakket 35</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>
+                    Pakket 30 rijlessen (60 min. & examen) Theorieboek €2015,-
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Pakket 35</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>
+                    Pakket 30 rijlessen (60 min. & examen) Theorieboek €2015,-
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Pakket 35</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>
+                    Pakket 30 rijlessen (60 min. & examen) Theorieboek €2015,-
+                  </p>
+                </CardContent>
+              </Card>
+            </div>
+            <h2>Overig</h2>
+            <div className="grid grid-cols-2 gap-4">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Pakket 35</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>
+                    Pakket 30 rijlessen (60 min. & examen) Theorieboek €2015,-
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Pakket 35</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>
+                    Pakket 30 rijlessen (60 min. & examen) Theorieboek €2015,-
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Pakket 35</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>
+                    Pakket 30 rijlessen (60 min. & examen) Theorieboek €2015,-
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Pakket 35</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>
+                    Pakket 30 rijlessen (60 min. & examen) Theorieboek €2015,-
+                  </p>
+                </CardContent>
+              </Card>
+            </div>
+            <h2>Theorie</h2>
+            <div className="grid grid-cols-2 gap-4">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Pakket 35</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>
+                    Pakket 30 rijlessen (60 min. & examen) Theorieboek €2015,-
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Pakket 35</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>
+                    Pakket 30 rijlessen (60 min. & examen) Theorieboek €2015,-
+                  </p>
+                </CardContent>
+              </Card>
+            </div>
+            <span>
+              Prijzen vanaf 5 februari 2024, wijzigingen onder voorbehoud.
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
